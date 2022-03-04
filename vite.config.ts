577b6c0b88739950b9d9dfa000a8523466e9d4f5
喵大快救我 <wang0122xl@163.com>
2022-03-04 14:35:34 +0800

    :wrench: cli

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Date: 2022-02-28 15:12:47
  * @Author: wang0122xl@163.com
  * @LastEditors: wang0122xl@163.com
- * @LastEditTime: 2022-03-01 17:34:11
+ * @LastEditTime: 2022-03-04 14:31:24
  * @Description: file content
  */
 import { defineConfig } from 'vite'
@@ -22,6 +22,9 @@ export default defineConfig({
                 javascriptEnabled: true,
             }
         }
+    },
+    build: {
+        sourcemap: true
     },
     plugins: [
         react(),
