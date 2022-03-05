226456d8e95f38261eb6364ad5f8cd65b8d26180
喵大快救我 <wang0122xl@163.com>
2022-03-05 13:47:29 +0800

    :sparkles: some changes

--- libs/tools/CircleTool.ts
+++ libs/tools/CircleTool.ts
@@ -2,13 +2,13 @@
  * @Date: 2022-02-24 15:58:06
  * @Author: wang0122xl@163.com
  * @LastEditors: wang0122xl@163.com
- * @LastEditTime: 2022-03-04 14:57:20
+ * @LastEditTime: 2022-03-05 13:46:54
  * @Description: file content
  */
 
-import P5BaseTool, { P5ToolAnnotation } from './baseTool'
+import P5BaseTool, { P5ToolAnnotation } from 'libs/tools/baseTool/index'
 import P5 from 'p5'
-import { CursorPoint, distanceBetween } from '../utils/index'
+import { CursorPoint, distanceBetween } from 'libs/utils/index'
 
 interface CircleToolAnnotation extends P5ToolAnnotation<'CircleTool'> {
 
