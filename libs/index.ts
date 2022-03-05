fa1239b0ce2e6fedcc55ee320c01bc1cb00d43d9
喵大快救我 <wang0122xl@163.com>
2022-03-05 15:46:02 +0800

    :wrench: confg ci publish

---
+++ libs/index.ts
@@ -0,0 +1,11 @@
+/*
+ * @Date: 2022-03-05 14:56:47
+ * @Author: wang0122xl@163.com
+ * @LastEditors: wang0122xl@163.com
+ * @LastEditTime: 2022-03-05 15:00:38
+ * @Description: file content
+ */
+
+import P5ToolsManager from './manager'
+
+export default P5ToolsManager
---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+/*
+ * @Date: 2022-03-05 15:30:26
+ * @Author: wang0122xl@163.com
+ * @LastEditors: wang0122xl@163.com
+ * @LastEditTime: 2022-03-05 15:30:26
+ * @Description: file content
+ */
+
+declare module '*.png' 
